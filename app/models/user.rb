f0affae67cfcfe760cb04d53029b52420f74592d
super-sausage <69895997+super-sausage@users.noreply.github.com>
2020-10-24 12:31:41 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+
+  has_many :rooms, through: :room_users
+  has_many :room_users
+
 end
